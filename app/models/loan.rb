309a922de1ee11221f5a7308d7c3cbc6d5e34f2a
msureshm <sureshmurthygari@gmail.com>
2024-08-25 20:25:55 +0530

    Added enum state

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,12 @@ class Loan < ApplicationRecord
   belongs_to :user
   has_many :loan_event_logs, dependent: :destroy
 
+  enum :state, { requested: 0,
+                  approved: 1,
+                  open: 2,
+                  closed: 3,
+                  rejected: 4,
+                  waiting_for_adjustment_acceptance: 5,
+                  readjustment_requested: 6 }
+
 end
