0748be8ca2a2e323dc675d64527a863c77a87de4
msureshm <sureshmurthygari@gmail.com>
2024-08-25 23:22:41 +0530

    Updating admin and client wallets after loan is aproved and confirmed

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -10,4 +10,19 @@ class Loan < ApplicationRecord
                   waiting_for_adjustment_acceptance: 5,
                   readjustment_requested: 6 }
 
+  after_update :transfer_amount_if_open
+
+  private
+
+  def transfer_amount_if_open
+    if open?
+      trasfer_amount
+    end
+  end
+
+  def trasfer_amount
+    admin = User.admin.first
+    admin.wallet.update(amount: admin.wallet.amount - principal_amount)
+    user.wallet.update(amount: user.wallet.amount + principal_amount)
+  end
 end
