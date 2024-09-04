8a39c555fbd8e4958644af9ddb1021b3df8e8a89
msureshm <sureshmurthygari@gmail.com>
2024-09-04 16:06:25 +0530

    Fixed automatic loan closing feature

--- app/sidekiq/loan_worker.rb
+++ app/sidekiq/loan_worker.rb
@@ -7,15 +7,16 @@ class LoanWorker
     admin_wallet = User.admin.first.wallet
     Loan.open.find_each do |loan|
       user_wallet = loan.user.wallet
-      if loan.total_amount_to_pay > user_wallet.amount
-        admin_wallet.update(amount: admin_wallet.amount + user_wallet.amount)
-        user_wallet.update(amount: 0)
-        loan.closed!
-      elsif loan.next_interest_time <= Time.zone.now
+      if loan.next_interest_time <= Time.zone.now
         loan.next_interest_time = loan.next_interest_time + 5.minutes
         interest = loan.principal_amount * loan.interest_rate / 100
         loan.interest_amount = loan.interest_amount + interest
         loan.save
+      end
+      if loan.total_amount_to_pay > user_wallet.amount
+        admin_wallet.update(amount: admin_wallet.amount + user_wallet.amount)
+        user_wallet.update(amount: 0)
+        loan.closed!
       end
     end
   end
